515b55d11a5b174751dc83f0951a39acf90a7c6d
SunnySiddique <devsunnywithcoding@gmail.com>
2026-06-18 01:32:18 +0600

    feat: implement cart schema with user reference and item details

---
+++ backend/models/cart.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    items: [
+      {
+        itemId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "FoodItem",
+          required: true,
+        },
+
+        name: {
+          type: String,
+          required: true,
+        },
+
+        price: {
+          type: Number,
+          required: true,
+        },
+
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+          default: 1,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Cart = mongoose.model("Cart", cartSchema);
+
+export default Cart;
